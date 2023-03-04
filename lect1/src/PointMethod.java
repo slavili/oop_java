17f8d3f2669d61f7a979679b05350ab42a0868df
Slavchik Slavili <slavchikv@rambler.ru>
2023-03-04 11:58:37 +0700

    Point2D class implementation

--- lect1/src/PointMethod.java
+++ lect1/src/PointMethod.java
@@ -1,13 +1,27 @@
+
 public class PointMethod {
     static double distance(int x1, int y1, int x2, int y2){
         return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
     }
+    static double distance(Point2D a, Point2D b){
+        return Math.sqrt(Math.pow(b.x-a.x, 2) + Math.pow(b.y-a.y,2));
+    }
 
     public static void main(String[] args) {
-        int ax = 0;
-        int ay = 0;
-        int bx = 0;
-        int by = 1;
-        System.out.println(distance(ax, ay, bx, by));
-    }
+
+        Point2D a = new Point2D();
+        a.x = 0;
+        a.y = 2;
+        System.out.println(a.toString());
+
+        Point2D b = new Point2D();
+        b.x = 0;
+        b.y = 10;
+        System.out.println(b.toString());
+
+        System.out.println(distance(a, b));
+        }
+
+
+
 }
