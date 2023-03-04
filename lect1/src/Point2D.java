eeb1c4bfa1791f3c9d79552067a3df8f339421fb
Slavchik Slavili <slavchikv@rambler.ru>
2023-03-04 13:31:00 +0700

    getInfo method implementation

--- lect1/src/Point2D.java
+++ lect1/src/Point2D.java
@@ -1,3 +1,21 @@
 public class Point2D {
     int x, y;
+    public Point2D(int valueX, int valueY){
+        this.x = valueX;
+        this.y = valueY;
+    }
+    public Point2D(int value){
+        this.x = value;
+        this.y = value;
+    }
+    public Point2D(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public String getInfo(){
+        return String.format("x: %d, y: %d", this.x, this.y);
+    }
+
+
 }
--- lect1/src/PointMethod.java
+++ lect1/src/PointMethod.java
@@ -9,19 +9,16 @@ public class PointMethod {
 
     public static void main(String[] args) {
 
-        Point2D a = new Point2D();
-        a.x = 0;
-        a.y = 2;
+        Point2D a = new Point2D(0,2);
         System.out.println(a.toString());
 
         Point2D b = new Point2D();
-        b.x = 0;
-        b.y = 10;
+
         System.out.println(b.toString());
 
         System.out.println(distance(a, b));
+
+        System.out.println(a.getInfo());
+        System.out.println(b.getInfo());
         }
-
-
-
 }
